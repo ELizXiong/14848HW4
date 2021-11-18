a724898f2a2b06de614816965c88ef9cbd65eea1
ELizXiong <60122319+ELizXiong@users.noreply.github.com>
2021-11-18 02:00:20 -0500

    formatted codes

--- codes/temp_mapper.py
+++ codes/temp_mapper.py
@@ -8,8 +8,5 @@ for line in sys.stdin:
     date = int(line[15:23])
     temperature = int(line[87:92])
     quality = int(line[92:93])
-    #sys.stdout.write(str(quality)+'\n')
-    #print(line, year, month, day, temperature, quality)
     if temperature != 9999 and quality in quality_included:
-        #sys.stdout.write(str(temperature)+'\n')
         print('%s\t%d' % (date, temperature))
